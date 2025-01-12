bbff6b968c8d162ff6adc6b20fe722d7d4a1bc7a
jay <1815820794@qq.com>
2025-01-12 23:21:19 +0800

    搭建了基本home页面

--- src/components/exercise.vue
+++ src/components/exercise.vue
@@ -15,11 +15,29 @@
     </el-table>
   </template>
   
-  <script setup>
+  <script setup lang="ts">
   import { ref } from 'vue';
   
   const tableData = ref([
     { status: '未完成', title: '2275.按位与结果大于零的最长组合', solutionCount: 167, passRate: '70.7%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '1.两数之和', solutionCount: 26388, passRate: '54.5%', difficulty: '简单', frequency: '' },
+    { status: '未完成', title: '2.两数相加', solutionCount: 14557, passRate: '44.8%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '3.无重复字符的最长子串', solutionCount: 17349, passRate: '40.6%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '4.寻找两个正序数组的中位数', solutionCount: 8370, passRate: '42.9%', difficulty: '困难', frequency: '' },
+    { status: '未完成', title: '5.最长回文子串', solutionCount: 9322, passRate: '39.0%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '6.Z字形变换', solutionCount: 5719, passRate: '53.5%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '1.两数之和', solutionCount: 26388, passRate: '54.5%', difficulty: '简单', frequency: '' },
+    { status: '未完成', title: '2.两数相加', solutionCount: 14557, passRate: '44.8%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '3.无重复字符的最长子串', solutionCount: 17349, passRate: '40.6%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '4.寻找两个正序数组的中位数', solutionCount: 8370, passRate: '42.9%', difficulty: '困难', frequency: '' },
+    { status: '未完成', title: '5.最长回文子串', solutionCount: 9322, passRate: '39.0%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '6.Z字形变换', solutionCount: 5719, passRate: '53.5%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '1.两数之和', solutionCount: 26388, passRate: '54.5%', difficulty: '简单', frequency: '' },
+    { status: '未完成', title: '2.两数相加', solutionCount: 14557, passRate: '44.8%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '3.无重复字符的最长子串', solutionCount: 17349, passRate: '40.6%', difficulty: '中等', frequency: '' },
+    { status: '完成', title: '4.寻找两个正序数组的中位数', solutionCount: 8370, passRate: '42.9%', difficulty: '困难', frequency: '' },
+    { status: '未完成', title: '5.最长回文子串', solutionCount: 9322, passRate: '39.0%', difficulty: '中等', frequency: '' },
+    { status: '未完成', title: '6.Z字形变换', solutionCount: 5719, passRate: '53.5%', difficulty: '中等', frequency: '' },
     { status: '完成', title: '1.两数之和', solutionCount: 26388, passRate: '54.5%', difficulty: '简单', frequency: '' },
     { status: '未完成', title: '2.两数相加', solutionCount: 14557, passRate: '44.8%', difficulty: '中等', frequency: '' },
     { status: '未完成', title: '3.无重复字符的最长子串', solutionCount: 17349, passRate: '40.6%', difficulty: '中等', frequency: '' },
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,38 @@
 <template>
-    <Headbar index="4"/>
-    <Exercise></Exercise>
+  <el-affix :offset="0">
+    <Headbar index="4" />
+  </el-affix>
+  <div class="main-box">
+    <el-container class="container">
+      <el-main class="main">        建议卡片
+        <div class="recommand-cards">
+  
+          <el-card style="max-width: 480px" />
+          <el-card style="max-width: 480px" />
+          <el-card style="max-width: 480px" />
+        </div>
+        <Exercise />
+      </el-main>
+      <el-aside class="aside"> 热门题目 </el-aside>
+    </el-container>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import Exercise from '@/components/exercise.vue';
-import Headbar from '@/components/headbar.vue';
+import Exercise from "@/components/exercise.vue";
+import Headbar from "@/components/headbar.vue";
 </script>
+<style scoped>
+.main-box {
+  display: flex;
+}
+.recommand-cards{
+    display:flex;
+}
+.el-card{
+    width: 200px;
+    height: 200px;
+    margin: auto;
+    margin-bottom: 35px;
+}
+</style>
